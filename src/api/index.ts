14a988ad68690e17e1069d2ea27fdf94258f6646
matosr96 <19matos96@gmail.com>
2023-03-03 09:14:42 -0500

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const VITE_API = axios.create({
-  // baseURL: 'https://real-vision-api.herokuapp.com'
-  // baseURL: 'https://real-vision-api-mono.onrender.com/api/v3'
-  //baseURL: 'https://partiaf-api.xyz/api/v3'
-  // baseURL: "http://44.203.116.120:5000/api/v3/health-check",
-  baseURL: "http://localhost:4200/api/v1",
+  baseURL: 'https://finanzea-backend-production.up.railway.app/api/v1'
+  //baseURL: "http://localhost:4200/api/v1",
 });
